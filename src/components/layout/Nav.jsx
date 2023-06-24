52c3d7f3d123ea350485d972b9b583f0a12a333b
frsty <wladimirburgos42@gmail.com>
2023-06-24 04:38:39 -0400

    cambios nav css

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -4,11 +4,18 @@ import '../styles/nav.css'
 
 export const Nav = () => {
   return (
-    <nav className="nav">
-      <ul>
-        <li><NavLink to={"/inicio"}>Ultimos</NavLink></li>
-        <li><NavLink to={"/crear-articulos"}>Publicar</NavLink></li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav">
+        <h1>セツナキ</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Publicacion, autor" />
+        </div>
+        <ul>
+          <li><NavLink to={"/inicio"}>Ultimos</NavLink></li>
+          <li><NavLink to={"/crear-articulos"}>Publicar</NavLink></li>
+        </ul>
+        
+      </nav>
+    </>
   )
 }
